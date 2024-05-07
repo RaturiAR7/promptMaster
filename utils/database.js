091c335337ca03e27afcbb2b1017f2d4dbec288b
Anshul <anshulraturi007@gmail.com>
2024-05-07 15:32:04 +0530

    Fix

--- utils/database.js
+++ utils/database.js
@@ -1,22 +1,29 @@
 import mongoose from "mongoose";
 
-let isConnected = false; //////Track the connection status
+let isConnected = false;
 
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
+
+  // If already connected, return early
   if (isConnected) {
-    console.log("Mongodb is already connected");
+    console.log("MongoDB is already connected");
     return;
   }
+
   try {
+    // Connect to MongoDB
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "share_prompt",
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     isConnected = true;
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error);
+    // Handle connection errors
+    console.error("Error connecting to MongoDB:", error);
+    isConnected = false; // Ensure isConnected is false in case of failure
   }
 };
